89b08e739b1c3330362b89f087f9efc7eadc50ec
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-06-30 09:55:41 +0300

    Update test_Q2.py

--- Question2/test_Q2.py
+++ Question2/test_Q2.py
@@ -4,11 +4,13 @@ from Q2 import lword
 from Q2 import cat
 
 
-class TestLeapYearTests:
+class TestLength:
 
 
  def test_length_of_words(self):
   assert lword(str(cat)) == True
+  
+  
 
 if __name__ == '__main__':
  pytest.main()
