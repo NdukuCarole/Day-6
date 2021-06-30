2823fcef947e45c3eb01be7f885c4b7fdd8360c5
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-06-30 05:46:14 +0300

    Add files via upload

---
+++ Question5/question5.py
@@ -0,0 +1,8 @@
+#listing the duplicated data
+d = [1,1,2,3,2,2,4,5,6,2,1]
+
+#using set() to convert to distinct elements
+x = set(d)
+
+#output after removal of duplicates
+print([x])
---
+++ Question5/test_Q5.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+from question5 import d
+from question5 import x
+
+def f():
+    raise SystemExit
+
+      
+def test_This_numbers(monkeypatch):
+   
+ assert d != x   
+
+if __name__ == '__main__':
+    pytest.main()  
