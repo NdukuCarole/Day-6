c0054c2b9bcd56c24d10d66c179e2d1f52022265
kahlinah <54279456+kahlinah@users.noreply.github.com>
2021-06-30 05:42:30 +0300

    Add files via upload

---
+++ Question4/test_Q4.py
@@ -0,0 +1,18 @@
+import pytest
+import sys
+
+
+from Question4 import info
+from Question4 import column
+
+
+class TestLeapYearTests:
+      
+
+
+  def test_csv(self):
+   
+   assert True; info !=column
+
+if __name__ == '__main__':
+    pytest.main()  
